b6923b5e0b1730074e159d201f54e51b3473ea06
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-06-09 10:23:17 +0100

    added developer notes and comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of villains Academy"
 puts "--------------"
 puts "Dr. Hannibal Lecter"
@@ -11,4 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+# finally, we print the total number of student. 
+print "overall, we have "
+#It's importht that print() doesn't add new line characters
+print 11
+puts " great students"
