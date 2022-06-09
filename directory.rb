e319393b30491311ce8504d195b7bc66af34d860
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-06-09 10:40:23 +0100

    sorted into methods

--- directory.rb
+++ directory.rb
@@ -16,13 +16,22 @@ students = [
 ]
 
 #and then print them
-puts "The students of villains Academy"
-puts "--------------"
-students.each do |student|
-  puts student 
+def print_header
+  puts "The students of villains Academy"
+  puts "------------"
 end
 
-# finally, we print the total number of student. 
-puts "overall, we have #{students.count} great students "
-#It's importht that print() doesn't add new line characters
+def print(names)
+  names.each do |name|
+    puts name 
+  end
+end
 
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
